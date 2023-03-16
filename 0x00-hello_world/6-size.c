e035b7e63383056ea311c2b0d9401e052475f652
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-16 06:25:45 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,7 @@ int main(void)
       	long long int c;
       	char d;
       	float f;
+
      	printf("Size of a char: %d byte(s)\n", sizeof(d));
       	printf("Size of an int: %d byte(s)\n", sizeof(a));
       	printf("Size of a long int: %d byte(s)\n", sizeof(b));
