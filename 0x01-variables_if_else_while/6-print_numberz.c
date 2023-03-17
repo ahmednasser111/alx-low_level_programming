cc8e87224180ca5dccb2ad52842173e489e4b9bc
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-17 11:31:49 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = '0'
+	for (; c < '10'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
