d57538b826ed4ea665bf46b6dc3f5ef7d250e73e
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-21 09:58:10 -0700

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,9 @@
 #include "main.h"
-/**   * print_alphabet - Make the alphabet   *   * Return: void   */
+/**
+ * print_alphabet - Make the alphabet   
+ *
+ * Return: void
+ */
 void print_alphabet(void)
 {
 	char c = 'a';
