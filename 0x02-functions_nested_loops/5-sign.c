f76f93e1476b5eb041a469af929e01e0222127c1
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-21 11:21:51 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Checks for alphabetic character
+ * @c: The character to be checked
+ *
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
+int print_sign(int n);
+{
+	if (n == 0)
+		return (0);
+	if (n > 0)
+		return (1);
+	return (-1);
+}
