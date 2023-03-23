d0318f122423d0165dc32920d24f8850c7cdedb5
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-23 04:20:23 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,12 +12,10 @@ void more_numbers(void)
 	for (; j < 10; j++)
 	{
 		i = 0;
-		for (; i < 10; i++)
-			_putchar(i + '0');
-		for (i = 0; i < 5; i++)
-		{
-			_putchar('1');
-			_putchar(i + '0');
+		for (; i < 15; i++)
+			if (i > 9)
+				_putchar('1');
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
