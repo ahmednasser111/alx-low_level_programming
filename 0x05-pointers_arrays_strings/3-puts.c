f03b773a8f7558ef5a65a8974115055743e74ca2
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-27 07:57:06 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*s)
+	{
+		_putchar(s);
+		s++;
+	}
+	_putchar('\n');
+}
