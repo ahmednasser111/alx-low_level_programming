8e6c09f5b7b271185e78514ff6081dda1853a305
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-27 08:53:02 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /**
  * rev_string - get a string in reverse order
@@ -7,17 +8,9 @@
  */
 void rev_string(char *s)
 {
-	int n = 0, i = 0;
-	char *sb = *s;
+	int n = strlen(s), i = 0;
 
-	while (*s)
-	{
-		n++;
-		s++;
-	}
 	for (; i < n / 2; i++)
-	{
-		*(sb + i) = *(sb - n - i - 1);
-	}
+		s[i] = s[n - i - 1];
 	putchar('\n');
 }
