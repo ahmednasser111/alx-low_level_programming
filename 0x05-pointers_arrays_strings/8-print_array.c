1f09c507e76f057d4e69dd37150c918c33999d78
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-27 14:19:13 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+/**
+ * print_array - prints an array
+ * @a: array
+ * @n: size
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	for (; i < n; i++)
+		printf("%d", a[i]);
+	putchar('\n');
+}
