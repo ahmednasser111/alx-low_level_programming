b9b5aa5e72d0da24928c0536f55d47c24c854d1f
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-27 20:20:32 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - copy a string to another string
+ * @dest: string
+ * @src: string
+ * Return: copy of string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, n = strlen(src);
+
+	for (; i < n; i++)
+		dest[i] = src[i];
+	*dest = '\0';
+	return (dest);
+}
