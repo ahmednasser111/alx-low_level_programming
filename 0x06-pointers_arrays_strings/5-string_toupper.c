e4635da3eeb7bc2b6b2353dbebb8f93b4afa6ebc
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-29 22:36:57 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * string_toupper - uppercase the string
  * @s: string
@@ -10,7 +11,7 @@ char *string_toupper(char *s)
 {
 	int i = 0;
 
-	for (; i < n; i++)
+	for (; i < strlen(s); i++)
 		if (s[i] > 96 && s[i] < 123)
 			s[i] -= 32;
 	return (s);
