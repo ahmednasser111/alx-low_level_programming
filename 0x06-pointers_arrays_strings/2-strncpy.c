0f99b221589f2913a164087a7c3d22a62f7d784f
ahmednasser111 <ahmednaser7707@gmail.com>
2023-03-29 22:03:26 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strncpy - copy a string to another
+ * @dest: string one
+ * @src: string two
+ * @n: size
+ *
+ * Return: A pointer to a string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+	for (; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
