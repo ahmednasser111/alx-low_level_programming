85b119326e1dea8cd4d390ba2369e75e58f107ef
ahmednasser111 <ahmednaser7707@gmail.com>
2023-04-05 08:23:03 -0700

    1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -10,10 +10,8 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		putchar('\n');
 		return;
 	}
-	if (*s != '\n')
-		_print_rev_recursion(++s);
+	_print_rev_recursion(++s);
 	putchar(*s);
 }
