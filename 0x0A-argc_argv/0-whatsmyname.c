3ebcfdf1fcdad52a7a5f9b9b3ff8e259d928d89a
ahmednasser111 <ahmednaser7707@gmail.com>
2023-04-05 20:52:26 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - main method
+ * @argc: size of argv
+ * @argv: array of commands
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+}
