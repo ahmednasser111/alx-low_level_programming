ae88b7c8f3d694bd6240d6079626968ee1f1c4c2
ahmednasser111 <ahmednaser7707@gmail.com>
2023-04-07 08:34:30 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main method
+ * @argc: size of argv
+ * @argv: array of commands
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
