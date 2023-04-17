159ffec19532d1cd3fd90658ab1faaa59d717089
ahmednaser111 <ahmednaser7707@gmail.com>
2023-04-17 18:26:12 +0200

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints the name of the source file it was
+ * compiled from to the standard output, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void) {
+    printf("%s\n", __FILE__);
+    return (0);
+}
