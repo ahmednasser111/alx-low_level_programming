77d1448901859ecc8b7097152b1c68ed22a77564
ahmednaser111 <ahmednaser7707@gmail.com>
2023-04-18 06:37:44 +0200

    0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef _DOG_H
+#define _DOG_H
+
 /**
  * struct dog - Dog attributes
  * @name: The name of the dog.
@@ -18,3 +21,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+#endif
