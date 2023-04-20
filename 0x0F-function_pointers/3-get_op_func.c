3be3ae0d718e7acd1a758b4a148768d1a40ae96f
ahmednaser111 <ahmednaser7707@gmail.com>
2023-04-20 15:10:00 +0200

    3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,5 @@
 #include "3-calc.h"
-
+#include <string.h>
 /**
  * get_op_func - Selects the correct function to perform the operation
  *               asked by the user
@@ -22,7 +22,7 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op)
 	{
-		if (*(ops[i].op) == *s && !(*(s + 1)))
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "3-calc.h"
 
 /**
@@ -13,35 +14,34 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	char operator;
+	char *operator;
 	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
-		printf("Error: Wrong number of arguments\n");
+		printf("Error\n");
 		exit(98);
 	}
 
 	num1 = atoi(argv[1]);
-	operator= argv[2][0];
+	operator= argv[2];
 	num2 = atoi(argv[3]);
 
 	op_func = get_op_func(operator);
 
-	if (op_func == NULL || argv[2][1] != '\0')
+	if (op_func == NULL)
 	{
-		printf("Error: Invalid operator '%s'\n", argv[2]);
+		printf("Error\n");
 		exit(99);
 	}
 
-	if ((operator== '/' || operator== '%') && num2 == 0)
+	if ((strcmp(operator, "/") == 0 || strcmp(operator, "%") == 0) && num2 == 0)
 	{
-		printf("Error: Can't divide/modulo by zero\n");
+		printf("Error\n");
 		exit(100);
 	}
-
 	result = op_func(num1, num2);
-	printf("%d %c %d = %d\n", num1, operator, num2, result);
+	printf("%d\n", result);
 
 	return (0);
 }
