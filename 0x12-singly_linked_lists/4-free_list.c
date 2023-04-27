4b57954e1e127d25aec4d3cd0de444da618e62c7
ahmednaser111 <ahmednaser7707@gmail.com>
2023-04-27 16:43:43 +0200

    4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,11 +8,12 @@
 void free_list(list_t *head)
 {
       list_t *temp;
-      while (head != NULL)
+      while (head)
       {
             temp = head;
             head = head->next;
             free(temp->str);
             free(temp);
       }
+      free(head);
 }
