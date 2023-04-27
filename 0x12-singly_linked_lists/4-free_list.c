1abb597a7920b72c6356ff331bd22f1351873145
ahmednaser111 <ahmednaser7707@gmail.com>
2023-04-27 16:40:29 +0200

    3

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * free_list - Frees a list_t list.
+ * @head: A pointer to the beginning of the list_t list.
+ *
+ * Return: Nothing.
+ */
 void free_list(list_t *head)
 {
       list_t *temp;
