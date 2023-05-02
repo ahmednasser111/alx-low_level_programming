e94caeecda2099bdd0aa9f641a5d3fd36871caea
ahmednaser111 <ahmednaser7707@gmail.com>
2023-05-02 06:29:58 +0300

    0

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 /**
- * print_listint - print a linked list of integers
+ * listint_len - get the size of a list
  *
  * @h: a pointer to the linked list
  * Returns: the size of nodes in the list
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h);
 {
 	size_t c = 0;
 
 	while (h)
 	{
 		c++;
-		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return c;
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 /**
  * struct listint_s - singly linked list
  * @n: integer
