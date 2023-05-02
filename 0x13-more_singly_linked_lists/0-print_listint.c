229f07a47f7299ac21d91ded8b1f1efea4a46773
ahmednaser111 <ahmednaser7707@gmail.com>
2023-05-02 06:37:03 +0300

    1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
  * print_listint - print a linked list of integers
  *
  * @h: a pointer to the linked list
- * Returns: the size of nodes in the list
+ * Return: the size of nodes in the list
  */
 size_t print_listint(const listint_t *h)
 {
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,9 +3,9 @@
  * listint_len - get the size of a list
  *
  * @h: a pointer to the linked list
- * Returns: the size of nodes in the list
+ * Return: the size of nodes in the list
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t c = 0;
 
