0a212a21b3a888261360e4ed5d3314588daf806b
ahmednaser111 <ahmednaser7707@gmail.com>
2023-05-03 18:44:08 +0300

    9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * insert_nodeint_at_index - ...
  * @head: ...
@@ -9,28 +10,38 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *temp;
-	int i = 1;
+	listint_t *new_node, *iter;
+	unsigned int i = 1;
 
 	if (head == NULL)
 		return (NULL);
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
 		return (NULL);
-	new->n = n;
+
+	new_node->n = n;
 	if (idx == 0)
 	{
-		new->next = *head;
-		*head = new;
-		return (new);
+		new_node->next = *head;
+		*head = new_node;
 	}
-	temp = *head;
-	for (; temp != NULL; temp = temp->next, i++)
-		if (i == idx)
+	else
+	{
+		iter = *head;
+		while (iter)
 		{
-			new->next = temp->next;
-			temp->next = new;
-			return (new);
+			if (i == idx)
+			{
+				new_node->next = iter->next;
+				iter->next = new_node;
+				return (new_node);
+			}
+			iter = iter->next;
+			i++;
 		}
+		if (idx > i)
+			return (NULL);
+	}
+	return (new_node);
 	return (NULL);
 }
