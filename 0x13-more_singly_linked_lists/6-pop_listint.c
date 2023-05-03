cc52412ab9fa4eda0e59a21e21389f90689f383e
ahmednaser111 <ahmednaser7707@gmail.com>
2023-05-03 17:55:11 +0300

    6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+int pop_listint(listint_t **head)
+{
+	listint_t new_head;
+
+	if (*head == NULL)
+		return 0;
+	new_head = (*head)->next;
+	n = (*head)->n;
+	free(*head);
+	*head = new_head;
+	return (n);
+}
