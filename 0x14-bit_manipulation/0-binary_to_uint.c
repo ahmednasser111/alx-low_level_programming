c1fbd772e482ba44ae17112ec6a8e771425e523b
ahmednaser111 <ahmednaser7707@gmail.com>
2023-05-04 14:11:38 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,45 @@
 #include "main.h"
+
 /**
- * binary_to_uint - Convert binary to unsigned int
- * @b: a binary number
+ * binary_to_uint - Converts a binary number to an unsigned int
+ * @b: The binary string to converts
  *
- * Return: an unsigned int
+ * Return: The positive number converted from a binary
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int n, uint = 0, i = 0;
+	unsigned int len = 0, count = 0, sum = 0;
 
 	if (b == NULL)
 		return (0);
-	n = _strlen(b);
-	for (i = 0; i < n; i++)
+
+	len = _strlen(b);
+	while (len--)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[len] != 48 && b[len] != 49)
 			return (0);
-		if (b[i] == '1')
-			uint += 1 << i;
+
+		if (b[len] == 49)
+			sum += 1 << count;
+
+		count++;
 	}
-	return (uint);
+
+	return (sum);
 }
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
 int _strlen(const char *s)
 {
 	int c = 0;
 
 	while (s[c])
 		c++;
+
 	return (c);
 }
