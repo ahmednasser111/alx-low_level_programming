18b1b1a5e5ce390811ee175509182563794d0d2c
ahmednaser111 <ahmednaser7707@gmail.com>
2023-05-04 15:37:47 +0300

    1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,7 +10,7 @@ void print_binary(unsigned long int n)
 {
 	if (n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return;
 	}
 	_divide(n);
@@ -28,7 +28,7 @@ void _divide(unsigned long int n)
 		return;
 	_divide(n >> 1);
 	if (n & 1)
-		_putchar('1');
+		putchar('1');
 	else
-		_putchar('0');
+		putchar('0');
 }
