acc4e5ed4bd245002fd0d8d6dd4e61532788b5e3
ahmednaser111 <ahmednaser7707@gmail.com>
2023-05-04 12:45:36 +0300

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,20 +7,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	if (b == NULL)
-		return (0);
 	unsigned int n = strlen(b), uint = 0, i = 0;
 
-	for (; i < n; i++)
+	if (b == NULL)
+		return (0);
+	for (i = 0; i < n; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		uint += pow(2, i) * (b[n - i - 1] - '0');
 	}
 	return (uint);
-}
-int main()
-{
-	char *s = "1010";
-	printf("%d", binary_to_uint(s));
 }
