90ecae5c4ef9637ce1b14fe43be0d6e15588e786
ahmednaser111 <ahmednaser7707@gmail.com>
2023-05-05 18:41:51 +0300

    2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,19 +11,14 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while (n)
-	{
+	for (; n; n /= 2, i++)
 		if (i == index)
 		{
 			if (n % 2)
 				return (1);
 			return (0);
 		}
-
-		n = n / 2;
-		i++;
-	}
-	if (index > i)
+	if (index > i && index < 63)
 		return (0);
 	return (-1);
 }
